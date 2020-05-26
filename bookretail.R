19ae6f620bc30089b40a65e4b73c0dd061e1a143
Aditya Sinha <55815239+adityasinha1812@users.noreply.github.com>
2020-05-25 20:35:42 -0500

    Update bookretail.R

--- bookretail.R
+++ bookretail.R
@@ -2,6 +2,7 @@
 library(dplyr)
 library(MASS)
 library(car)
+library(glmnet)
 
 # read data
 orders = read.csv("C:/Users/adity/Downloads/Courses/Spring 2020/MSIT 423 - Data Science/Kaggle Competition/orders.csv")
@@ -55,8 +56,22 @@ div_cat_df = orders %>%
   group_by(id) %>%
   summarise(div_cat = n_distinct(category))
 
+# entropy (diversity in amount of purchases from all categories (~0.5 means diverse))
+entropy_df = orders %>%
+  group_by(id, category) %>%
+  summarise(total_qty = sum(qty)) %>% 
+  mutate(ent_freq = total_qty / sum(total_qty)) %>%
+  group_by(id) %>% 
+  summarise(entropy = -sum(ent_freq * log(ent_freq)))
+
+avg_cat_df = orders %>%
+  group_by(id, ordnum) %>%
+  summarise(cat_count_ord = n_distinct(category)) %>% 
+  group_by(id) %>%
+  summarise(avg_cat = mean(cat_count_ord))
+
 # combining all data frames
-list_df = list(tot_items_df, freq_df, mon_df, total_spend_df, div_cat_df, tof_df, rec_df)
+list_df = list(tot_items_df, freq_df, mon_df, total_spend_df, div_cat_df, tof_df, rec_df, entropy_df, avg_cat_df)
 rfm_df = Reduce(function(x, y) merge(x, y, all.x = TRUE), list_df)
 
 # checking stats for rfm created
@@ -74,14 +89,13 @@ master_data = merge(rfm_df, target, by = "id", all.x = TRUE)
 train = !is.na(master_data$logtarg)
 
 # fit simple linear regression
-fit_1 = lm(logtarg ~ freq_item + freq_order + log(avg_qty + 1) + log(avg_price + 1) + log(total_cust_spend + 1) + div_cat + tof + rec, data = master_data, subset = train)
+fit_1 = lm(logtarg ~ log(freq_item + 1) + freq_order + log(avg_qty + 1) + log(avg_price + 1) + log(total_cust_spend + 1) + div_cat + tof + rec + entropy + avg_cat, data = master_data, subset = train)
 summary(fit_1)
 vif(fit_1)
-plot(fit_1)
 yhat_1 = predict(fit_1, master_data[!train,])
 ans_1 = data.frame(id = master_data$id[!train], logtarg = yhat_1)
 ans_1$logtarg = ifelse(ans_1$logtarg < 0, 0, ans_1$logtarg)
-write.csv(ans_1, "C:/Users/adity/Downloads/Courses/Spring 2020/MSIT 423 - Data Science/Kaggle Competition/testanswer_1.csv", row.names = F)
+write.csv(ans_1, "C:/Users/adity/Downloads/Courses/Spring 2020/MSIT 423 - Data Science/Kaggle Competition/testanswer_3.csv", row.names = F)
 
 # fit stepwise backward
 fit_2 <- stepAIC(fit_1, direction = c("backward"))
@@ -90,4 +104,15 @@ vif(fit_2)
 yhat_2 = predict(fit_2, master_data[!train,])
 ans_2 = data.frame(id = master_data$id[!train], logtarg = yhat_2)
 ans_2$logtarg = ifelse(ans_2$logtarg < 0, 0, ans_2$logtarg)
-write.csv(ans_2, "C:/Users/adity/Downloads/Courses/Spring 2020/MSIT 423 - Data Science/Kaggle Competition/testanswer_2.csv", row.names = F)
+write.csv(ans_2, "C:/Users/adity/Downloads/Courses/Spring 2020/MSIT 423 - Data Science/Kaggle Competition/testanswer_4.csv", row.names = F)
+
+# lasso regression
+options(na.action="na.pass")
+x = model.matrix(logtarg ~ .-id, master_data)
+fit.lasso = glmnet(x[train, ], master_data$logtarg[train], alpha = 1)
+plot(fit.lasso, xvar="lambda")
+fit_3.cv = cv.glmnet(x[train, ], master_data$logtarg[train], alpha = 1, nfold = 3, lambda = seq(0, 10, 0.01))
+yhat_3 = predict(fit.lasso, s = fit_3.cv$lambda.min, newx = x[!train, ])
+ans_3 = data.frame(id = master_data$id[!train], logtarg = yhat_3)
+ans_3$X1 = ifelse(ans_3$X1 < 0, 0, ans_3$X1)
+write.csv(ans_3, "C:/Users/adity/Downloads/Courses/Spring 2020/MSIT 423 - Data Science/Kaggle Competition/testanswer_5.csv", row.names = F)
